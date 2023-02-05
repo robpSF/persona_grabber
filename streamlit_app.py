2d65f141e7b8718ac2cb6afdbff30553ff7c5121
robpSF <robert@tstoryteller.com>
2023-02-05 11:13:46 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ import datetime
 from datetime import timezone
 from geopy.geocoders import Nominatim
 
-geolocator = Nominatim(user_agent="robsapp")
+geolocator = Nominatim(sl.secrets["user_agent"])
 
 import sys
 
